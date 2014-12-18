8bfba0c4584bff3d0b958847109723c9678ab307
Mark Redar <mredar@gmail.com>
2014-12-17 16:32:23 -0800

    More work on cuke & robot

--- BDD/cuke-test/features/step_definitions/get_marc_input_field_steps.rb
+++ BDD/cuke-test/features/step_definitions/get_marc_input_field_steps.rb
@@ -1,14 +1,24 @@
+require 'json';
+
 Given(/^that (\S+).xml has field (\S+) and the value is (.+)$/) do |infile, field, value|
+    @value = value
+    #need to change infile to grab of data
     print('INFOO=>', infile, ' field=>', field, ' val=>', value, "\n")
 end
 
 When(/^I look at (\S+)$/) do |resultFile|
     print('RESULT FILE=>', resultFile, "\n")
+    #need to change open file to grab of live data
+    @json_result = JSON.load(File.open(resultFile, "r"))
+    print("srcRes title->"+@json_result['sourceResource']['title'].inspect)
+    #json read file
+    #save json object
 end
 
 Then(/^(\S+) should be (.+)$/) do | jsonpath, value |
     print("\nJSONP=>", jsonpath, " expected value", value, "\n")
-    raise "how do I get earlier values?"
+    found_value = @json_result['sourceResource']['title'].inspect
+    print("srcRes title->"+found_value)
 end
 
 
