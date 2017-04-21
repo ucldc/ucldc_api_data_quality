ed535d9f9c560e7499fb7ac56fe14d3d60a0461e
Mark Redar <mredar@gmail.com>
2017-04-21 14:00:25 -0700

    Working, now to add more cmd line opts

--- reporting/check_mediajson.py
+++ reporting/check_mediajson.py
@@ -1,92 +1,183 @@
+import sys
 try:
     import configparser
 except:
     import ConfigParser as configparser
+import logging
 import json
+import argparse
 import csv
 import boto3
 
 from get_solr_json import get_solr_json
 
-if __name__ == "__main__":
-    config = configparser.SafeConfigParser()
-    config.read('report.ini')
-    solr_url = config.get('new-index', 'solrUrl')
-    api_key = config.get('new-index', 'solrAuth')
-    print('SOLR: {}'.format(solr_url))
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def get_solr_docs(solr_url, api_key, query=None):
+    '''Get just the documents for a given query'''
     cursorMark = '*'
-    query = {
-        'q': 'structmap_url:[* TO *]',
-        'fl': 'id, structmap_url, collection_url',
-        'rows': 1000,
-        'cursorMark': cursorMark,
-        'sort': 'id asc'
-    }
-    struct_map_urls = []
+    if not query:
+        query = {
+            'q': 'structmap_url:[* TO *]',
+            'fl': 'id, structmap_url, collection_url, type_ss',
+            'rows': 1000,
+            'cursorMark': cursorMark,
+            'sort': 'id asc'
+        }
+    docs = []
     num_read = 0
     solr_json = get_solr_json(solr_url, query, api_key=api_key)
     while len(solr_json['response']['docs']) > 0:
         cursorMark = solr_json['nextCursorMark']
         query['cursorMark'] = cursorMark
-        struct_map_urls.extend(solr_json['response']['docs'])
+        docs.extend(solr_json['response']['docs'])
         num_read += len(solr_json['response']['docs'])
         if (num_read % 5000) == 0:
-            print(num_read)
+            logger.info(num_read)
         solr_json = get_solr_json(solr_url, query, api_key=api_key)
-    with open('structmap_urls.json', 'w') as foo:
-        json.dump(struct_map_urls, foo, indent=2)
+    return docs
 
-    print('\n\n{} nuxeo objects in Solr\n\n'.format(num_read))
-    print('\n\nGet object list from S3\n\n')
 
-    s3 = boto3.resource('s3')
+def get_summary_objects(s3, bucket, prefix):
+    bucket = s3.Bucket(bucket)
+    return bucket.objects.filter(Prefix=prefix)
+
+
+def get_media_json_keys(s3):
     # gather list of media json files "keys"
     # keys will be 'media_json/<UUID>-media.json'
-    bucket = s3.Bucket('static.ucldc.cdlib.org')
-    media_json_summary_objs = bucket.objects.filter(Prefix='media_json')
+    media_json_object_summaries = get_summary_objects(
+        s3, 'static.ucldc.cdlib.org', 'media_json')
     media_json_keys = []
     count = 0
-    for objsum in media_json_summary_objs:
+    for objsum in media_json_object_summaries:
         count += 1
         if (count % 5000) == 0:
-            print(count)
+            logger.info(count)
         media_json_keys.append(objsum.key)
+    return media_json_keys
 
-    print('\n\nTotal number of media_json in s3:'
-          '{}\n\n'.format(len(media_json_keys)))
-
-    bad_media = []
-    count = 0
-    bad_count = 0
-    for row in struct_map_urls:
-        count += 1
-        # if (count % 1000) == 0:
-        #    print('Processed: {}'.format(count))
-        bucket, folder, key = row['structmap_url'].rsplit('/', 2)
-        #x, bucket = bucket.rsplit('/', 1)
-        s3key = '{}/{}'.format(folder, key)
-        if s3key not in media_json_keys:
-            bad_count += 1
-            if (bad_count % 1000) == 0:
-                print('{} bad so far'.format(bad_count))
-            bad_media.append(row)
 
 # this was REALLY slow
 #        obj_summary = s3.ObjectSummary(bucket, '{}/{}'.format(folder, key))
 #        try:
 #            obj_summary.size
 #        except botocore.exceptions.ClientError as e:
-#            print('missing media: {}'.format(row))
-#            bad_media.append(row)
+#            logger.info('missing media: {}'.format(row))
+#            missing_media.append(row)
 
-    bad_media_sorted = sorted(bad_media, key=lambda x: x['collection_url'])
 
-    print('{} missing media_json files'.format(len(bad_media_sorted)))
-    with open('bad_media.json', 'w') as foo:
-        json.dump(bad_media_sorted, foo, indent=2)
+def missing_media_json(s3, solr_docs):
+    missing_media = []
+    count = 0
+    bad_count = 0
+    for row in solr_docs:
+        count += 1
+        bucket, folder, key = row['structmap_url'].rsplit('/', 2)
+        s3key = '{}/{}'.format(folder, key)
+        if s3key not in media_json_keys:
+            bad_count += 1
+            if (bad_count % 1000) == 0:
+                logger.info('{} bad so far'.format(bad_count))
+            missing_media.append(row)
+    return missing_media
 
-    with open('bad_media.csv', 'w') as csvfile:
+
+def get_jp2000_file_sizes(s3):
+    '''Get the sizes and UUID for all of the jp2000 files on s3
+    Return a dictionary indexed by UUID'''
+    jp2000_object_summaries = get_summary_objects(s3, 'ucldc-private-files',
+                                                  'jp2000')
+    uuid_sizes = {}
+    for objsum in jp2000_object_summaries:
+        folder, UUID = objsum.key.rsplit('/', 1)
+        uuid_sizes[UUID] = objsum.size
+    return uuid_sizes
+
+
+def get_missing_jp2000_docs(s3, solr_docs):
+    '''
+    Build a list of all the docs with missing or 0 size jp2000
+    '''
+    uuid_sizes = get_jp2000_file_sizes(s3)
+    problems = []
+    for doc in solr_docs:
+        print('checking doc : {}'.format(doc['id']))
+        if doc.get('type_ss') == ['text']:
+            continue
+        UUID = doc['id']
+        if not uuid_sizes.get(UUID):
+            if uuid_sizes.get(UUID) is None:
+                doc['size'] = -1
+            else:
+                doc['size'] = uuid_sizes[UUID]
+            problems.append(doc)
+    return problems
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--outdir', default='reports',
+            help='out directory for reports (defaults to reports)')
+    parser.add_argument('--inisection', default='new-index',
+            help='section of report.ini to get Solr server info')
+
+    if argv is None:
+        argv = parser.parse_args()
+
+    print('INI SECTOIN={}'.format(argv.inisection))
+    config = configparser.SafeConfigParser()
+    config.read('report.ini')
+    solr_url = config.get(argv.inisection, 'solrUrl')
+    api_key = config.get(argv.inisection, 'solrAuth')
+    print('SOLR: {}'.format(solr_url))
+    nuxeo_solr_docs = get_solr_docs(solr_url, api_key)
+    with open('structmap_urls.json', 'w') as foo:
+        json.dump(nuxeo_solr_docs, foo, indent=2)
+
+    print('\n\n{} nuxeo objects in Solr\n\n'.format(
+        len(nuxeo_solr_docs)))
+
+    print('\n\nGet object list from S3\n\n')
+    s3 = boto3.resource('s3')
+    media_json_keys = get_media_json_keys(s3)
+    print('\n\nTotal number of media_json in s3:'
+          '{}\n\n'.format(len(media_json_keys)))
+
+    missing_media = missing_media_json(s3, nuxeo_solr_docs)
+    missing_media_sorted = sorted(
+        missing_media, key=lambda x: x['collection_url'])
+
+    print('{} missing media_json files'.format(
+        len(missing_media_sorted)))
+    with open('missing_media.json', 'w') as foo:
+        json.dump(missing_media_sorted, foo, indent=2)
+
+    with open('missing_media.csv', 'w') as csvfile:
         writer = csv.writer(csvfile)
-        for obj in bad_media_sorted:
+        for obj in missing_media_sorted:
             writer.writerow((obj['collection_url'], obj['id'],
-                             obj['structmap_url']))
+                             obj['structmap_url'], obj.get('type_ss')))
+
+    missing_jp2000 = get_missing_jp2000_docs(s3, nuxeo_solr_docs)
+    missing_jp2000_sorted = sorted(
+        missing_jp2000, key=lambda x: x['collection_url'])
+    print('{} missing jp2000 files'.format(
+        len(missing_media_sorted)))
+    with open('missing_jp2000.json', 'w') as foo:
+        json.dump(missing_jp2000_sorted, foo, indent=2)
+    with open('missing_jp2000.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for obj in missing_jp2000_sorted:
+            writer.writerow(
+                (obj['collection_url'], obj['id'], obj['structmap_url'],
+                 obj.get('type_ss'), obj['size']))
+
+if __name__ == "__main__":
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.DEBUG)
+    logger.addHandler(sh)
+
+    sys.exit(main())
